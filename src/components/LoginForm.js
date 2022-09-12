78afa1c87a970c2afd55eccd4762ec15573172d2
Jake Street <streetjake@gmail.com>
2022-09-12 13:04:20 -0400

    added password recovery and email verification added user info being stored into firebase database

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,14 +3,16 @@ import '../App.css'
 import { auth, app } from './utils/firebase'
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
-import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from 'firebase/auth';
 
 
 export default function LoginForm() {
     const emailInputRef = useRef();
     const passwdInputRef = useRef();
+    const fNameInputRef = useRef();
+    const lNameInputRef = useRef();
     
-    const [user, setUser] = useState({});
+    const [localUser, setUser] = useState({});
 
     const [loginStatus, setLoginStatus] = useState("");
 
@@ -18,35 +20,38 @@ export default function LoginForm() {
       setUser(currentUser);
     })
 
-    //const db = getFirestore(app);
+    const db = getFirestore(app);
 
     const SignUpForm = async (e)=>{
         e.preventDefault();
         const email = emailInputRef.current.value;
         const password = passwdInputRef.current.value;
+        const firstName = fNameInputRef.current.value;
+        const lastName = lNameInputRef.current.value;
 
         try {
         const user = await createUserWithEmailAndPassword(auth, email, password);
         console.log(user);
-        } catch (error) {
-          setLoginStatus(error.message);
-        }
-
-        /*
-        const docRef = doc(db, "users", user);
+        await sendEmailVerification(user.user);
+        const docRef = doc(db, "users", user.user.uid);
         const docSnap = await getDoc(docRef);
 
         if (!docSnap.exists()) {
-          setDoc(doc(db, "users", email), {
-            username: email,
-            hashedPassword: hashedpassword
+          setDoc(doc(db, "users", user.user.uid), {
+            email: email,
+            firstname: firstName,
+            lastname: lastName,
+            role: "user"
           });
           setLoginStatus("Registration Successful!")
         }
         else {
           setLoginStatus("Username already in use!")
         }
-        */
+        } catch (error) {
+          setLoginStatus(error.message);
+        }
+
 
     }
 
@@ -56,33 +61,25 @@ export default function LoginForm() {
         const password = passwdInputRef.current.value;
         
         try {
-          const user = await signInWithEmailAndPassword(auth, email, password)
+          await signInWithEmailAndPassword(auth, email, password)
           setLoginStatus("Successfully Logged In!")
         } catch (error) {
           setLoginStatus(error.message);
         }
-        /*
-        const docRef = doc(db, "users", email);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          if(result) {
-            setLoginStatus("Logged in!");
-          } 
-          else {
-            setLoginStatus("Incorrect password!");
-          }
-        } 
-        else {
-          // doc.data() will be undefined in this case
-          setLoginStatus("This username does not exist!");
-        }
-        */
     }
 
     const LogoutForm = async (e)=>{
       await signOut(auth);
   }
+
+      const ResetForm = async (e)=>{
+        try {
+          await sendPasswordResetEmail(auth, localUser.email);
+          } catch (error) {
+            setLoginStatus(error.message);
+          }
+        
+    }
 
     return (
         <div>
@@ -91,12 +88,17 @@ export default function LoginForm() {
                 <input type="text" id="uname" name="uname" ref={emailInputRef}/>
                 <label htmlFor="passwd">Password:</label>
                 <input type="password" id="passwd" name="passwd" ref={passwdInputRef}/>
+                <label htmlFor="fname">First Name:</label>
+                <input type="text" id="fname" name="fname" ref={fNameInputRef}/>
+                <label htmlFor="lname">Last Name:</label>
+                <input type="text" id="lname" name="lname" ref={lNameInputRef}/>
                 <button type="submit" onClick={LoginForm}>Log In</button>
                 <button type="submit" onClick={LogoutForm}>Log Out</button>
                 <button type="button" onClick={SignUpForm}>Register</button>
+                <button type="button" onClick={ResetForm}>Reset Password</button>
             </form>
             <h1>Response: {loginStatus}</h1>
-            <h1>Current User: {user?.email}</h1>
+            <h1>Current User: {localUser?.email}</h1>
         </div>
     )
 }
