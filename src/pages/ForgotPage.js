61b9c32ad6166478a21f5738889c1ead01e3b55b
Jake Street <streetjake@gmail.com>
2022-09-26 18:03:02 -0400

    security questions

--- src/pages/ForgotPage.js
+++ src/pages/ForgotPage.js
@@ -3,6 +3,7 @@ import {
     MDBContainer,
     MDBInput,
     MDBBtn,
+    MDBCardText,
   }
 from 'mdb-react-ui-kit';
 import { useAuth } from '../contexts/AuthContext';
@@ -10,25 +11,40 @@ import { Alert } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Collapse from '@mui/material/Collapse';
 import CloseIcon from '@mui/icons-material/Close'
+import { useNavigate } from 'react-router-dom';
+import { app } from '../components/utils/firebase'
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 export default function ForgotPage() {
     const emailInputRef = useRef();
+    const emailQuestionsInputRef = useRef();
+    const userInputRef = useRef();
     const [message, setMessage] = useState("");
     const [alertSeverity, setAlertSeverity] = useState("success");
     const [open, setOpen] = useState(false);
-    const { forgotPassword } = useAuth();
+    const { forgotPassword, setResetEmail, setResetUser, setQuestionOne, setQuestionOneAnswer, setQuestionTwo, setQuestionTwoAnswer, resetUser } = useAuth();
+    const navigate = useNavigate();
+    const db = getFirestore(app);
     
-    const SendResetEmail = async (e)=>{
-        await forgotPassword(emailInputRef.current.value)
-        .then(response => {
-          setMessage("Reset Link Succesfully Sent!")
-          setAlertSeverity("success")
-          setOpen(true)
-        }).catch(e => {
-          setMessage(e.message)
-          setAlertSeverity("warning")
-          setOpen(true)
-        })
+
+
+    const NavigateToQuestions = async (e)=>{
+      setResetEmail(emailQuestionsInputRef.current.value)
+      setResetUser(userInputRef.current.value)
+      const questionInfo = await GetQuestions()
+      navigate("/answer-questions")
+    }
+
+    async function GetQuestions() {
+      const docRef = doc(db, "users", userInputRef.current.value);
+      const docSnap = await getDoc(docRef);
+  
+      setQuestionOne(docSnap.data().questionOne)
+      setQuestionTwo(docSnap.data().questionTwo)
+      console.log(docSnap.data().questionTwoAnswer)
+      setQuestionOneAnswer(docSnap.data().questionOneAnswer)
+      setQuestionTwoAnswer(docSnap.data().questionTwoAnswer)
+  
     }
 
     const SendAlert = (e)=>{
@@ -63,6 +79,10 @@ export default function ForgotPage() {
           <h1>Forgot Password</h1>
           <MDBInput wrapperClass='mb-4 w-50 m-auto mt-4' label='Email address' id='resetEmail' type='email' inputRef={emailInputRef}/>
           <MDBBtn onClick={SendResetEmail}className="mb-4 w-25 m-auto mt-2">Send Reset Link</MDBBtn>
+          <MDBCardText className='m-auto'>Or</MDBCardText>
+          <MDBInput wrapperClass='mb-2 w-50 m-auto mt-4' label='Username' id='resetEmail' type='email' inputRef={userInputRef}/>
+          <MDBInput wrapperClass='mb-4 w-50 m-auto mt-2' label='Email address' id='resetEmail' type='email' inputRef={emailQuestionsInputRef}/>
+          <MDBBtn onClick={NavigateToQuestions}className="mb-4 w-25 m-auto mt-2">Get Security Questions</MDBBtn>
           <MDBBtn className="mb-4 w-25 m-auto" href="/" color="link">Return to Log In</MDBBtn>
         </MDBContainer>
       </div>
